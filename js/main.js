d8cb87e7d8e238da02fc7b4f52a809f76570ef7c
Travis Kupp <travis.kupp@gmail.com>
2019-06-02 12:57:07 -0400

    Move static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Biography",
+    'image': {
+      'desc': "headshot photo of Travis Kupp",
+      'src': "images/linkedin_only-4.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Manifesto",
+    'image': {
+      'desc': "example screenshot of an undefined project",
+      'src': "images/example2.png",
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+                  https://www.flickr.com/photos/ssoosay/4097410999`
+    }
+  },
+  {
+    'title': "Coming Soon",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
